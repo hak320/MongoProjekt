3c318dff52f818edda793f238238b746380856dd
hak320 <hakan320@hotmail.com>
2020-03-11 11:09:26 +0000

    test1

--- server.js
+++ server.js
@@ -15,10 +15,21 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {    useNewUrlParser: true, useUnifiedTopology: true})
-const db = mongoose.connetion
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+mongoose.connect(process.env.DATABASE_URL, {    useNewUrlParser: true, useUnifiedTopology: true} ,function(err,db){
+  if(err){
+    console.log('errrrr')
+  }else{
+    const db = mongoose.connetion
+    db.on('error', error => console.error(error))
+    db.once('open', () => console.log('Connected to Mongoose'))
+    console.log('gut')
+  }
+})
+
+//mongoose.connect('mongodb://127.0.0.1:27017/MyBrary', {    useNewUrlParser: true, useUnifiedTopology: true})
+//const db = mongoose.connetion
+//db.on('error', error => console.error(error))
+//db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
