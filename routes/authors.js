d3c5d62f66fefd06bad159a53c9a78f8a41cbc59
hak320 <hakan320@hotmail.com>
2020-03-13 10:52:45 +0000

    Books Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 
 })
 
-// New Autjor Route
+// New Author Route
 router.get('/new', (req, res) => {
     res.render('authors/new', { author: new Author() })
 })
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
 const express = require('express')
@@ -9,32 +9,23 @@ const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs')
-app.set('views',__dirname + '/views')
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {    useNewUrlParser: true, useUnifiedTopology: true} ,function(err,db){
-  if(err){
-    console.log('errrrr')
-  }else{
-    const db = mongoose.connetion
-    db.on('error', error => console.error(error))
-    db.once('open', () => console.log('Connected to Mongoose'))
-    console.log('gut')
-  }
-})
-
-//mongoose.connect('mongodb://127.0.0.1:27017/MyBrary', {    useNewUrlParser: true, useUnifiedTopology: true})
-//const db = mongoose.connetion
-//db.on('error', error => console.error(error))
-//db.once('open', () => console.log('Connected to Mongoose'))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
