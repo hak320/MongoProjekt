476f02e4629df232cb54d6e91ccdf7c63cf2c847
hak320 <hakan320@hotmail.com>
2020-03-18 13:11:09 +0000

    Finish Project

--- routes/books.js
+++ routes/books.js
@@ -146,7 +146,7 @@ async function renderFormPage(res, book, form, hasError = false) {
 }
 
 function saveCover(book, coverEncoded) {
-  if (coverEncoded == null) return 
+  if (coverEncoded == null) return
   const cover = JSON.parse(coverEncoded)
   if (cover != null && imageMimeTypes.includes(cover.type)) {
     book.coverImage = new Buffer.from(cover.data, 'base64')
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
-const methodOverride = require('method-override')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -19,7 +19,6 @@ app.use(expressLayouts)
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
